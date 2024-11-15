2401b5cb7a550c632b4efab9b1fdf49a1c01298b
Chris Moynan <Chrismoynan@outlook.com>
2024-11-15 23:59:20 +0000

    added styling for welcome page and main content

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path(
-        '',
-        views.home,
-        name='home'
-    ),
+    path('', views.home, name='home'),  # Route to the home view
 ]
